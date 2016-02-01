aa22e3864024a25a980ea3dfc1955df6bb815568
chocoluffy <yushunzhe951104@gmail.com>
2016-02-01 14:52:21 -0500

    fix getElementById

--- game.js
+++ game.js
@@ -1,13 +1,11 @@
 function init(){
-	var touchzone = document.getElementById("#mycanvas");
-	touchzone = addEventListener("touchstart", draw, false);
-}
-
-function draw(event){
-	var canvas = document.getElementById("#mycanvas");
-	if(canvas.getContext){
-		var ctx = canvas.getContext("2d");
-		ctx.fillRect(event.touches[0].clientX, event.touches[0].clientY, 5, 5)
-
-	}
+	var touchzone = document.getElementById("mycanvas");
+	touchzone = addEventListener("touchstart", function(e){
+		var canvas = document.getElementById("mycanvas");
+		console.log("touched!!");
+		if(canvas.getContext){
+			var ctx = canvas.getContext("2d");
+			ctx.fillRect(e.touches[0].clientX, e.touches[0].clientY, 5, 5)
+		}
+	}, false);
 }
